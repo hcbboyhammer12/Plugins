bccd777af7d0f3e7cbe4d1acf09bfb7a6cf51859
hcbboyhammer12 <carson.thegamer@gmail.com>
2013-11-14 17:41:45 -0700

    Updating Enforcer

--- Enforcer/src/me/hcbboyhammer12/enforcer/Enforcer.java
+++ Enforcer/src/me/hcbboyhammer12/enforcer/Enforcer.java
@@ -1,7 +1,16 @@
 package me.hcbboyhammer12.enforcer;
 
+import org.bukkit.command.CommandSender;
+import org.bukkit.material.Command;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Enforcer extends JavaPlugin {
 
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("kick")) {
+
+
+        }
+        return true;
+    }
 }
